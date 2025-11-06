d8ef50fdbcd917e2f5e86e2b3edbf17c8585b448
moridmerdas-cpu <morid.merdas@gmail.com>
2025-11-06 13:19:04 -0800

    Create mother_bot.py

---
+++ mother_bot.py
@@ -0,0 +1,27 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from config import MOTHER_TOKEN, CHILD_TOKENS, OWNER_ID
+
+def start(update: Update, context: CallbackContext):
+    user_id = update.effective_user.id
+    if user_id != OWNER_ID:
+        update.message.reply_text("فقط مدیر می‌تونه از این ربات استفاده کنه ❌")
+        return
+
+    buttons = [
+        [InlineKeyboardButton(f"ربات فرعی {i+1}", url=f"https://t.me/ReactionChild{i+1}Bot")]
+        for i in range(len(CHILD_TOKENS))
+    ]
+
+    markup = InlineKeyboardMarkup(buttons)
+    update.message.reply_text("ربات‌های فرعی:", reply_markup=markup)
+
+def main():
+    updater = Updater(MOTHER_TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if name == "main":
+    main()
