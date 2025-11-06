18ba0026e341159d5af1ad79b0bb2fed7a71d73d
moridmerdas-cpu <morid.merdas@gmail.com>
2025-11-06 14:30:16 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 MOTHER_TOKEN = "8302315933:AAEmKDQ43wNci65t71E6tXaRCmf2tcJFqpc"
 
-CHILD_TOKENS = [
-    "8487304584:AAG40orObWv-_6g_6wifFc8BhkytmNdzfuA",
-    "7554850849:AAHJkb9KQbQKYBt7ustF17eU3EGg-CksYeY",  
-    # ...
+# لیست ربات‌های فرعی (username)
+CHILD_BOTS = [
+    "SoulVerseReaction1_bot",
+    "SoulVerseReaction2_bot",
+    # بقیه ربات‌ها رو همینجا اضافه کن تا 20 عدد
 ]
 
 OWNER_ID = 8321313612  # آی‌دی عددی خودت
